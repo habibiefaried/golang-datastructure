9762b6024581490b2e96862a421b4c7ee2656fd2
Habibie Faried <habibiefaried@gmail.com>
2019-12-29 21:20:44 +0700

    Adding stack

---
+++ stack-queue/stack.go
@@ -0,0 +1,84 @@
+package main
+/* 
+	Name: Habibie Faried
+	Date: 30 December 2019
+	Stack implementation
+*/
+
+import (
+	"fmt"
+)
+
+type Element struct {
+	body int
+	//You can add more element here....
+	next *Element
+}
+
+type Stack struct {
+	length	int
+	top		*Element
+}
+
+/* Initialize Stack*/
+func createStack() (*Stack) {
+	return &Stack{
+		length: 0,
+	}
+}
+/* */
+
+func (f *Stack) push(newElmt *Element) () {
+	if (f.length == 0){
+		f.top = newElmt
+	} else {
+		newElmt.next = f.top
+		f.top = newElmt
+	}
+	f.length++
+}
+
+func (f *Stack) pop() (*Element) {
+	if (f.length == 0){
+		return nil
+	} else {
+		temp := f.top
+		f.top = f.top.next
+		f.length--
+		return temp
+	}
+}
+
+func (f *Stack) getTop() (*Element) {
+	if (f.length == 0){
+		return nil
+	} else {
+		return f.top
+	}
+}
+
+func (f *Stack) isEmpty() (bool) {
+	if (f.top == nil){
+		return true
+	} else {
+		return false
+	}
+}
+
+func main(){
+	s := createStack()
+	i := 0
+	var e *Element
+	for i != 10 {
+		fmt.Println("Pushed ",i*3)
+		s.push(&Element{body: i*3})
+		i++
+	}
+	fmt.Println("-------------")
+
+	for !s.isEmpty() {
+		e = s.pop()
+		fmt.Println("Popped ",e.body)
+		
+	}
+}
